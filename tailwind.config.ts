94794e262bb8b2cdce33628b771de67d32452f9a
Stanley Lew <stanley43595@gmail.com>
2024-12-22 18:00:20 -0800

    add file structure and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        csa:{
+          "red-100": "#7C2413",
+          "red-200": "#842A35",
+          "green-100": "#6F77765",
+          "yellow-100": "#D0AD6B",
+          "yellow-200": "#E0C799",
+          "yellow-300": "#E9A545",
+          "tan-100": "#F9EDD7",
+          "tan-200": "#CBAD95",
+          "tan-300": "#AD9B85",
+          "gray-100": "#535353",
+          "gray-200": "#534C4A",
+        },
+      },
+    },
   },
   plugins: [],
 };
