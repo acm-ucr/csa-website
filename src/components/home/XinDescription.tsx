81455de3e4d244b31f22c464735661dd7b36f362
Simon Cun <simoncun2586@gmail.com>
2025-01-10 14:58:29 -0800

    Added Theme Description Component.

---
+++ src/components/home/XinDescription.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const XinDescription = () => {
+  return (
+    <div>
+      <p className="text-white text-left">
+        The Chinese character 心 ("xin") which stands for heart. The theme that
+        we have been promoting is "Two Worlds, One Heart” which is a
+        representation of our Chinese Heritage fused with the American Culture
+        that defines the Chinese American.
+      </p>
+    </div>
+  );
+};
+
+export default XinDescription;
