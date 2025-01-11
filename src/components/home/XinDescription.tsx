4a59d1015a6722c37949f75240d5507185ae2e4a
Simon Cun <simoncun2586@gmail.com>
2025-01-11 13:13:00 -0800

    Fixed width, background color, added borders, and updated text size

--- src/components/home/XinDescription.tsx
+++ src/components/home/XinDescription.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const XinDescription = () => {
   return (
-    <div>
-      <p className="text-left text-white">
+    <div className="box-sizing w-full border-y-8 border-csa-tan-300 bg-csa-green-100">
+      <p className="w-8/12 px-16 py-24 text-left font-serif text-4xl font-medium text-white">
         The Chinese character 心 ("xin") which stands for heart. The theme that
         we have been promoting is "Two Worlds, One Heart” which is a
         representation of our Chinese Heritage fused with the American Culture
