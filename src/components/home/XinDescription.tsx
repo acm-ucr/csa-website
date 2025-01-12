2f6bc6ea0d4eaa739dc7eaad47bdc2fee21da80c
Simon Cun <simoncun2586@gmail.com>
2025-01-12 12:40:18 -0800

    removed the w-full

--- src/components/home/XinDescription.tsx
+++ src/components/home/XinDescription.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const XinDescription = () => {
   return (
-    <div className="w-full border-y-8 border-csa-tan-300 bg-csa-green-100">
+    <div className="border-y-8 border-csa-tan-300 bg-csa-green-100">
       <p className="w-2/3 px-[4vw] py-[6vh] text-left text-2xl text-white">
         The Chinese character 心 ("xin") which stands for heart. The theme that
         we have been promoting is "Two Worlds, One Heart” which is a
