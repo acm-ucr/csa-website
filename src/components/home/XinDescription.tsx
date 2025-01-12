a58f2765bb0ee3f23634af91e7023b82002c9a07
Simon Cun <simoncun2586@gmail.com>
2025-01-12 12:14:42 -0800

    fixed border value and added component to page.tsx

--- src/components/home/XinDescription.tsx
+++ src/components/home/XinDescription.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const XinDescription = () => {
   return (
-    <div className="box-sizing w-full border-y-[2vh] border-csa-tan-300 bg-csa-green-100">
+    <div className="w-full border-y-8 border-csa-tan-300 bg-csa-green-100">
       <p className="w-2/3 px-[4vw] py-[16vh] text-left text-4xl text-white">
         The Chinese character 心 ("xin") which stands for heart. The theme that
         we have been promoting is "Two Worlds, One Heart” which is a
