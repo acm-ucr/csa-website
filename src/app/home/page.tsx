dc4e514b33deaee67f8ebce3b0f60c6aa82a856f
Sookie Drabla <sookie.sd@@gmail.com>
2025-01-13 12:40:33 -0800

    Add member experience title

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import MemberTitle from "@/components/home/MemberExperienceTitle";
+
+const page = () => {
+    return (
+    <div>
+        <MemberTitle />
+    </div>
+    )
+};
+export default page;
---
+++ src/components/home/MemberExperienceTitle.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MemberTitle = () => {
+    return (
+        <div className="flex flex-col">
+            <p className="mr-2 pr-4 text-right text-5xl font-serif text-csa-red-100">
+        Member Experiences
+        </p>
+        </div>
+    )
+};
+export default MemberTitle;
