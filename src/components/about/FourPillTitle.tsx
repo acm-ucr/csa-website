12ec55b11c3aca9aacc6abfa06953b8b1558743b
Brandon Tran <brandontran1220@gmail.com>
2025-01-12 17:31:19 -0800

    Add border under Pillars

--- src/components/about/FourPillTitle.tsx
+++ src/components/about/FourPillTitle.tsx
@@ -2,10 +2,11 @@ import React from "react";
 
 const Heading = () => {
   return (
-    <div className="flex justify-end">
-      <p className="me-10 w-fit border-b-4 border-b-csa-tan-300 text-5xl font-semibold text-csa-red-100">
+    <div className="flex flex-col">
+      <p className="mr-2 pr-4 text-right text-5xl font-semibold text-csa-red-100">
         Four Pillars
       </p>
+      <div className="ml-auto mr-4 h-[1vh] w-[11vw] bg-csa-tan-300"></div>
     </div>
   );
 };
