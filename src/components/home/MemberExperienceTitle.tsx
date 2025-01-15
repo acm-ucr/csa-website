2ff0eb7932a92df84513dd1bc3e94966c5a16c74
Sookie Drabla <sookie.sd@@gmail.com>
2025-01-15 11:10:12 -0800

    Remove font (with correct file included)

--- src/components/home/MemberExperienceTitle.tsx
+++ src/components/home/MemberExperienceTitle.tsx
@@ -1,9 +1,7 @@
-import React from "react";
-
 const MemberTitle = () => {
   return (
-    <div className="flex flex-col">
-      <p className="mr-2 pr-4 text-right font-serif text-5xl text-csa-red-100">
+    <div>
+      <p className=" mr-2 pr-4 text-right text-5xl text-csa-red-100">
         Member Experiences
       </p>
     </div>
