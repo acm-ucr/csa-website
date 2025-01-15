d7085708d5b8fff5d94b96cc66214cdd6122be05
xxiyun <wesleylwu@gmail.com>
2025-01-15 14:06:43 -0800

    reformatted with prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import EHTitle from "@/components/events/EventHighlightsTitle";
 
 const Home = () => {
   return (
-    <div className="flex-col h-screen w-screen items-center justify-center">
+    <div className="h-screen w-screen flex-col items-center justify-center">
       <EHTitle />
       <XinDescription />
     </div>
