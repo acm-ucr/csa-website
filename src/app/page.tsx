d13f5dbbd0c631cabca6eb41fcda9540691e78bf
Sookie Drabla <sookie.sd@@gmail.com>
2025-01-15 12:57:28 -0800

    run formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 import XinDescription from "@/components/home/XinDescription";
-import MemberTitle  from "@/components/home/MemberExperienceTitle";
+import MemberTitle from "@/components/home/MemberExperienceTitle";
 
 const Home = () => {
   return (
     <>
-    <div className="flex h-screen w-screen items-center justify-center">
-      <XinDescription />
-    </div>
-    
-    <div>
-      <MemberTitle />
-    </div>
-  </>
+      <div className="flex h-screen w-screen items-center justify-center">
+        <XinDescription />
+      </div>
+
+      <div>
+        <MemberTitle />
+      </div>
+    </>
   );
 };
 
--- src/components/home/MemberExperienceTitle.tsx
+++ src/components/home/MemberExperienceTitle.tsx
@@ -1,7 +1,7 @@
 const MemberTitle = () => {
   return (
     <div>
-      <p className=" mr-2 pr-4 text-right font-semibold text-5xl text-csa-red-100">
+      <p className="mr-2 pr-4 text-right text-5xl font-semibold text-csa-red-100">
         Member Experiences
       </p>
     </div>
