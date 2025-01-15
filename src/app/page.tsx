4ab2670dab75a52f7b30af72a92cb5253a6b37eb
xxiyun <wesleylwu@gmail.com>
2025-01-15 13:51:17 -0800

    moved component to page.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import XinDescription from "@/components/home/XinDescription";
+import EHTitle from "@/components/events/EventHighlightsTitle";
 
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="flex-col h-screen w-screen items-center justify-center">
+      <EHTitle />
       <XinDescription />
     </div>
   );
