d86e4545dce337dc0e60d64bd100e8a765cdffbd
Patrick Wu <89278486+exitcy@users.noreply.github.com>
2025-01-16 13:15:24 -0800

    made text slightly bigger

--- src/components/home/MissionStatement.tsx
+++ src/components/home/MissionStatement.tsx
@@ -4,17 +4,17 @@ const MissionStatement = () => {
   return (
     <div className="px-8 py-4">
       <div className="border-l-8 border-csa-tan-300 pl-6">
-        <p className="mb-4 text-left text-4xl font-bold text-csa-red-100">
+        <p className="mb-0 text-left text-5xl font-bold text-csa-red-100">
           Mission statement
         </p>
-        <p className="text-left text-lg leading-7 text-csa-gray-100">
-          Social, cultural, and service orientated organization
+        <p className="mt-4 text-left text-2xl leading-9 text-csa-gray-100">
+          Social, cultural, and service orientated
           <br />
-          that strives to promote the Chinese American culture
+          organization that strives to promote the Chinese
           <br />
-          throughout the UCR and Riverside
+          American culture throughout the UCR and
           <br />
-          communities.
+          Riverside communities.
         </p>
       </div>
     </div>
