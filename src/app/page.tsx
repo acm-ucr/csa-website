388707df648fb3fa07428d93f5db155859cc2c82
Patrick Wu <89278486+exitcy@users.noreply.github.com>
2025-01-17 11:53:31 -0800

    fixed more stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import XinDescription from "@/components/home/XinDescription";
-import MemberTitle from "@/components/home/MemberExperienceTitle";
-import MissionStatement from "@/components/home/MissionStatement";
+import QuoteBox from "@/components/home/QuoteBox";
+import quotes from "@/data/quote-data";
 import EHTitle from "@/components/events/EventHighlightsTitle";
+import MissionStatement from "@/components/home/MissionStatement";
 
 const Home = () => {
   return (
     <div className="bg-csa-tan-100 pt-[12.2vh] md:pt-[14.4vh]">
+      <MissionStatement />
       <EHTitle />
       <XinDescription />
-      <MissionStatement />
-      <MemberTitle />
+      {quotes.map(({ quote, person }, index) => (
+        <QuoteBox quote={quote} name={person} key={index} />
+      ))}
     </div>
   );
 };
