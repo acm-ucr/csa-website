99eb5fe49afca963d1754a834af7baf3f728a0e2
Sookie Drabla <sookie.sd@@gmail.com>
2025-01-20 14:29:06 -0800

    fix border color

--- src/components/about/Philanthropy.tsx
+++ src/components/about/Philanthropy.tsx
@@ -4,7 +4,7 @@ import Image from "@/public/about/heart.svg";
 const Philanthropy = () => {
   return (
     <div className="relative grid grid-cols-4 p-6">
-      <div className="col-span-2 flex items-center rounded-3xl border-8 border-csa-gray-200 p-2">
+      <div className="col-span-2 flex items-center rounded-3xl border-8 border-csa-green-100 bg-white p-2">
         <p className="font-lora p-4 text-left text-5xl font-bold text-csa-gray-200">
           Philanthropy
         </p>
