4bd971fdef4db06300755764d12b6d4167b69234
Sookie Drabla <sookie.sd@@gmail.com>
2025-01-20 14:21:26 -0800

    run format

--- src/components/about/Philanthropy.tsx
+++ src/components/about/Philanthropy.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import Image from "@/public/about/heart.svg";
 
 const Philanthropy = () => {
-    return (
-        <div className="relative grid grid-cols-4 p-6">
-            <div className="col-span-2 flex items-center border-8 rounded-3xl border-csa-gray-200 p-2">
-                <p className=" text-left text-5xl font-lora font-bold text-csa-gray-200 p-4">Philanthropy</p>
-                <div className="absolute left-1/2 " style={{transform: 'translateY(5%) translateX(-50%)'}}>
-                    <img src={Image.src} alt="heart" className="w-3/4 h-3/4" />
-                </div>
-            </div>
+  return (
+    <div className="relative grid grid-cols-4 p-6">
+      <div className="col-span-2 flex items-center rounded-3xl border-8 border-csa-gray-200 p-2">
+        <p className="font-lora p-4 text-left text-5xl font-bold text-csa-gray-200">
+          Philanthropy
+        </p>
+        <div
+          className="absolute left-1/2"
+          style={{ transform: "translateY(5%) translateX(-50%)" }}
+        >
+          <img src={Image.src} alt="heart" className="h-3/4 w-3/4" />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Philanthropy;
-    
