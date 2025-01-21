e1e08a194653fa45b13d30f68ec4febcf974635c
Brandon Tran <brandontran1220@gmail.com>
2025-01-20 23:18:04 -0800

    Optimize code and add correct image

--- src/components/about/Social.tsx
+++ src/components/about/Social.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 import Image from "next/image";
-import Lantern from "@/public/about/lantern.svg";
+import Pendant from "@/public/about/pendant.webp";
 
 const Social = () => {
   return (
-    <div className="relative ml-auto mr-16 flex h-[12vh] w-[25vw] items-center justify-center rounded-3xl bg-csa-gray-100">
-      <div className="absolute flex h-[10.5vh] w-[24.2vw] items-center rounded-3xl bg-white">
+    <div className="flex justify-end">
+      <div className="relative flex h-[12vh] w-[25vw] items-center rounded-3xl border-8 border-csa-gray-100">
         <p className="ml-8 text-3xl font-bold text-csa-gray-100">Social</p>
         <Image
-          src={Lantern}
-          alt="Lantern"
-          className="absolute right-5 w-24 -rotate-12"
+          src={Pendant}
+          alt="Pendant"
+          className="absolute right-0 w-[9vw]"
         />
       </div>
-      <div></div>
     </div>
   );
 };
