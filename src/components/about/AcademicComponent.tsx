3b658e438b7683481d1dcb4f2bfee7de3f1aeefb
xxiyun <wesleylwu@gmail.com>
2025-01-21 15:59:12 -0800

    decreased font size, increased width of div, and changed mr-28 to mr-[5vw]

--- src/components/about/AcademicComponent.tsx
+++ src/components/about/AcademicComponent.tsx
@@ -4,9 +4,9 @@ import gradCap from "@/public/about/gradCap.webp";
 
 const Academic = () => {
   return (
-    <div className="mr-28 flex justify-end">
-      <div className="relative flex h-[12vh] w-[25vw] items-center rounded-3xl border-8 border-csa-yellow-100">
-        <p className="ml-8 text-6xl font-bold text-csa-gray-100">Academic</p>
+    <div className="mr-[5vw] flex justify-end">
+      <div className="relative flex h-[12vh] w-[28vw] items-center rounded-3xl border-8 border-csa-yellow-100">
+        <p className="ml-8 text-3xl font-bold text-csa-gray-100">Academic</p>
         <Image
           src={gradCap}
           alt="gradCap"
