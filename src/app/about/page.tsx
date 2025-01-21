7744f9e58bcb4a75e6f6235e6f81a89648152496
Draylend <draylend0821@gmail.com>
2025-01-20 23:57:57 -0800

    Add latern on right

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,7 @@
 import WhoWeAre from "@/components/about/WhoWeAre";
 import Heading from "@/components/about/FourPillTitle";
 import Title from "@/components/Title";
+import RightLantern from "@/components/about/RightLantern";
 
 const page = () => {
   return (
@@ -8,6 +9,7 @@ const page = () => {
       <WhoWeAre />
       <Heading />
       <Title text="about" />
+      <RightLantern />
     </div>
   );
 };
