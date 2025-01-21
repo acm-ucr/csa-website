316bf36c95b10c8be04e457504a084e9a43e939c
Sookie Drabla <sookie.sd@@gmail.com>
2025-01-20 22:47:51 -0800

    run format

--- src/components/about/Philanthropy.tsx
+++ src/components/about/Philanthropy.tsx
@@ -4,13 +4,13 @@ import HeartImage from "@/public/about/heart.svg";
 
 const Philanthropy = () => {
   return (
-    <div className="flex m-6">
-      <div className="relative h-[12vh] w-[25vw] border-8 border-csa-green-100 rounded-3xl flex items-center">        
+    <div className="m-6 flex">
+      <div className="relative flex h-[12vh] w-[25vw] items-center rounded-3xl border-8 border-csa-green-100">
         <p className="font-lora p-4 text-5xl font-bold text-csa-gray-200">
           Philanthropy
         </p>
         <div
-          className="absolute right"
+          className="right absolute"
           style={{ transform: "translateY(5%) translateX(110%)" }}
         >
           <Image src={HeartImage} alt="heartImage" className="h-3/4 w-3/4" />
