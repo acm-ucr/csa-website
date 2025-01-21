6a83ba487c91b7182b85f57b1be077bb104451d0
Austin Le <ale193@ucr.edu>
2025-01-21 00:21:59 -0800

    Adding footer to site

---
+++ src/components/Footer.tsx
@@ -0,0 +1,36 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import React, { useState } from "react";
+import CsaLogo from "@/public/home/csalogo.svg";
+import { icons } from "@/data/footerData";
+
+const Footer = () => {
+  const [clicked, setClicked] = useState("Home");
+  return (
+    <div className="relative bottom-0 flex h-24 w-full items-center justify-between border-t-8 border-csa-yellow-100 bg-csa-red-200 align-bottom">
+      <Link
+        onClick={() => {
+          setClicked("Home");
+        }}
+        href="/"
+      >
+        <Image src={CsaLogo} alt="CSA_Logo" className="left-0 mx-5 w-20" />
+      </Link>
+
+      <div className="absolute right-0 flex w-[30%] justify-evenly md:w-[20%]">
+        {icons.map((icon, index) => (
+          <Link
+            href={icon.link}
+            key={index}
+            className="inline-flex items-center text-xl text-white hover:scale-150 hover:transform hover:cursor-pointer hover:text-csa-yellow-300"
+          >
+            {icon.image}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/data/footerData.tsx
@@ -0,0 +1,34 @@
+//import Image from "next/image";
+import { Instagram } from "lucide-react";
+import { Facebook } from "lucide-react";
+import { Youtube } from "lucide-react";
+import { Globe } from "lucide-react";
+import { Linkedin } from "lucide-react";
+
+export const icons = [
+  {
+    name: "instagram",
+    image: <Instagram />,
+    link: "https://www.instagram.com/ucrcsa/?hl=en",
+  },
+  {
+    name: "facebook",
+    image: <Facebook />,
+    link: "https://www.facebook.com/groups/UCRCSA/",
+  },
+  {
+    name: "youtube",
+    image: <Youtube />,
+    link: "https://www.youtube.com/@ucrcsa2386",
+  },
+  {
+    name: "globe",
+    image: <Globe />,
+    link: "https://highlanderlink.ucr.edu/organization/csa/",
+  },
+  {
+    name: "Linkedin",
+    image: <Linkedin />,
+    link: "https://www.linkedin.com/company/chinese-student-association/",
+  },
+];
