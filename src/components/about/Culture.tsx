8cc0ea2d6e6e5131ee57d683340d6f12ee8faab4
Patrick Wu <89278486+exitcy@users.noreply.github.com>
2025-01-21 16:38:44 -0800

    changed font, sizing, and over all dimensions of redenvelope

--- src/components/about/Culture.tsx
+++ src/components/about/Culture.tsx
@@ -4,15 +4,15 @@ import redenvelope from "@/public/about/envelope.webp";
 
 const Culture = () => {
   return (
-    <div className="ml-28 flex justify-start">
+    <div className="m-12 flex">
       <div className="relative flex h-[12vh] w-[25vw] items-center rounded-3xl border-8 border-csa-red-100 bg-white">
-        <p className="absolute right-6 text-5xl font-bold text-csa-gray-200">
+        <p className="font-lora absolute right-2 p-4 text-3xl font-bold text-csa-gray-200">
           Culture
         </p>
         <Image
           src={redenvelope}
           alt="Red Envelope"
-          className="absolute h-[340px] w-[230px] translate-x-[-40%] translate-y-[5%]"
+          className="absolute h-[300px] w-[260px] translate-x-[-34%] translate-y-[5%]"
         />
       </div>
     </div>
