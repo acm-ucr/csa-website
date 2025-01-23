13006cd0fda4c81dca876c076e39f07797165713
inegishi <inegishi5@gmail.com>
2025-01-22 16:36:31 -0800

    dragon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,22 @@ import QuoteBox from "@/components/home/QuoteBox";
 import quotes from "@/data/quote-data";
 import EHTitle from "@/components/events/EventHighlightsTitle";
 import MissionStatement from "@/components/home/MissionStatement";
-
+import Image from "next/image";
 const Home = () => {
   return (
-    <div className="bg-csa-tan-100 pt-[12.2vh] md:pt-[14.4vh]">
-      <MissionStatement />
+    <div className="bg-csa-tan-100">
+      <div className="grid grid-flow-col grid-rows-3 gap-4">
+        <Image
+          src="/home/dragon.png"
+          alt="dragon"
+          width={450}
+          height={450}
+          className="row-span-3"
+        />
+        <div className="col-span-2 row-span-2 row-start-3">
+          <MissionStatement />
+        </div>
+      </div>
       <EHTitle />
       <XinDescription />
       {quotes.map(({ quote, person }, index) => (
