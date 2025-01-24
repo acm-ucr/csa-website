2fe0289f782442d795e277db81a5450012f0bb2f
inegishi <inegishi5@gmail.com>
2025-01-23 16:11:16 -0800

    inegishi dragon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,23 +3,14 @@ import XinDescription from "@/components/home/XinDescription";
 import QuoteBox from "@/components/home/QuoteBox";
 import quotes from "@/data/quote-data";
 import EHTitle from "@/components/events/EventHighlightsTitle";
-import MissionStatement from "@/components/home/MissionStatement";
 import Image from "next/image";
+import Dragon from "@/public/home/dragon.png"
+
+
 const Home = () => {
   return (
-    <div className="bg-csa-tan-100">
-      <div className="grid grid-flow-col grid-rows-3 gap-4">
-        <Image
-          src="/home/dragon.png"
-          alt="dragon"
-          width={450}
-          height={450}
-          className="row-span-3"
-        />
-        <div className="col-span-2 row-span-2 row-start-3">
-          <MissionStatement />
-        </div>
-      </div>
+    <div className="bg-csa-tan-100  ">
+      <Image src = {Dragon} alt ='dragon' className=" w-1/4  left-0 top-0" />
       <EHTitle />
       <XinDescription />
       {quotes.map(({ quote, person }, index) => (
