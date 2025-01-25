500a7b08ef653f9c98c82af90d4479f0ae82c183
Ian Negishi <inegi001@ucr.edu>
2025-01-24 16:27:12 -0800

    new pic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,12 @@ import XinDescription from "@/components/home/XinDescription";
 import QuoteBox from "@/components/home/QuoteBox";
 import quotes from "@/data/quote-data";
 import Image from "next/image";
-import Dragon from "@/public/home/Dragon.png"
-
+import Dragon from "@/public/home/Dragon.png";
 
 const Home = () => {
   return (
-    <div className="bg-csa-tan-100 ">
-      <Image src = {Dragon} alt ='dragon' className="left-0 top-0 w-1/4" />
+    <div className="bg-csa-tan-100">
+      <Image src={Dragon} alt="dragon" className="left-0 top-0 w-1/4" />
       <XinDescription />
       {quotes.map(({ quote, person }, index) => (
         <QuoteBox quote={quote} name={person} key={index} />
