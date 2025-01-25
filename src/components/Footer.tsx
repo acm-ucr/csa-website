e009b64c6edf716e68719c8eb93b3be8687a67fa
Austin Le <ale193@ucr.edu>
2025-01-24 22:02:47 -0800

    Replacing lucide icons with react icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,13 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
-import React, { useState } from "react";
 import CsaLogo from "@/public/home/csalogo.svg";
 import { icons } from "@/data/footerData";
 
 const Footer = () => {
-  const [, setClicked] = useState("Home");
   return (
     <div className="relative bottom-0 flex h-24 w-full items-center justify-between border-t-8 border-csa-yellow-100 bg-csa-red-200 align-bottom">
       <Link
-        onClick={() => {
-          setClicked("Home");
-        }}
         href="/"
       >
         <Image src={CsaLogo} alt="CSA_Logo" className="left-0 mx-5 w-20" />
@@ -23,9 +18,9 @@ const Footer = () => {
           <Link
             href={icon.link}
             key={index}
-            className="inline-flex scale-[1.5] items-center text-xl text-white hover:scale-[2] hover:cursor-pointer hover:text-csa-yellow-300"
+            className="inline-flex items-center justify-center text-3xl text-white hover:text-5xl hover:cursor-pointer hover:text-csa-yellow-300"
           >
-            {icon.image}
+          {icon.image}
           </Link>
         ))}
       </div>
