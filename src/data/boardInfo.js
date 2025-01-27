264e7e61850ac003f867ecb01d90cd9b2d2e9c0c
Sookie Drabla <sookie.sd@@gmail.com>
2025-01-26 22:44:22 -0800

    add info

---
+++ src/data/boardInfo.js
@@ -0,0 +1,294 @@
+import Briana from "@/public\board\Briana_Lam.JPG";
+import Ryan_Lew from "@/public\board\Ryan_Lew.JPG";
+import Megan from "@/public\board\Megan_Ho.JPG";
+import Renne from "@/public\board\Renne_Sanchex.JPG";
+import Marcus from "@/public\board\Marcus_Lee.JPG";
+import Dominic from "@/public\board\Dominic_Rivera.JPG";
+import Alex from "@/public\board\Alex_Sin.JPG";
+import Irene from "@/public\board\Irene_Li.JPG";
+import Kyle from "@/public\board\Kyle_Chan.JPG";
+import Ingrid from "@/public\board\Ingrid_Yee.JPG";
+import Ryan_Hoang from "@/public\board\Ryan_Hoang.JPG";
+import Yoonah from "@/public\board\Yoonah_Choi.JPG";
+import Isabelle from "@/public\board\Isabelle_Huang.JPG";
+import Yuhan from "@/public\board\Yuhan_Shi.JPG";
+import Joshua from "@/public\board\Joshua_Yu.JPG";
+import Anson from "@/public\board\Anson_Gao.JPG";
+import Kathy from "@/public\board\Kathy_Liu.JPG";
+import Connor from "@/public\board\Connor_Lau.JPG";
+import Mia from "@/public\board\Mia_Chiang.JPG";
+import Samantha from "@/public\board\Samantha_Lee.JPG";
+import Nathan from "@/public\board\Nathan_Ceung.JPG";
+import Peter from "@/public\board\Peter_Hsia.JPG";
+import Andy from "@/public\board\Andy_Banh.JPG";
+import Kaelyn from "@/public\board\Kaelyn_Lam.JPG";
+import Aidan from "@/public\board\Aidan_Lam.JPG";
+// import Andrew from "@/public\board\Andrew_Zhang.JPG";
+import Caitlyn from "@/public\board\Caitlyn_Hang.JPG";
+import Ryan_Nguyen from "@/public\board\Ryan_Nguyen.JPG";
+import Stephanie from "@/public\board\Stephanie_Huang.JPG";
+
+
+export const boardInfo = [
+  {
+    image: Briana,
+    name: "Briana Lam",
+    title: "President",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Ryan_Lew,
+    name: "Ryan Lew",
+    title: "External Vice President",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Megan,
+    name: "Megan Ho",
+    title: "Internal Vice President",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Renne,
+    name: "Renne Sanchez",
+    title: "Financial Vice President",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Marcus,
+    name: "Marcus Lee",
+    title: "Senior Advisor",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  }, 
+  {
+    image: Dominic,
+    name: "Dominic Rivera",
+    title: "Senior Advisor",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  }, 
+  {
+    image: Alex,
+    name: "Alex Sin",
+    title: "Social Chair",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Irene,
+    name: "Irene Li",
+    title: "Social Chair",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Kyle,
+    name: "Kyle Chan",
+    title: "Public Relations Chair",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Ingrid,
+    name: "Ingrid Yee",
+    title: "Public Relations Chair",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Ryan_Hoang,
+    name: "Ryan Hoang",
+    title: "Fundraising Chair",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Yoonah,
+    name: "Yoonah Choi",
+    title: "Fundraising Chair",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Isabelle,
+    name: "Isabelle Huang",
+    title: "Design Chair",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Yuhan,
+    name: "Yuhan Shi",
+    title: "Design Chair",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Joshua,
+    name: "Joshua Yu",
+    title: "Media Chair",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Anson,
+    name: "Anson Gao",
+    title: "Media Chair",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Kathy,
+    name: "Kathy Liu",
+    title: "Family Chair",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Connor,
+    name: "Connor Lau",
+    title: "Family Chair",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Mia,
+    name: "Mai Chiang",
+    title: "Chinese Culture Night Director",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Samantha,
+    name: "Samantha Lee",
+    title: "Chinese Culture Night Scriptwriter",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Nathan,
+    name: "Nathan Ceung",
+    title: "Chinese Culture Night Producer",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Peter,
+    name: "Peter Hsia",
+    title: "Chinese Culture Night Stage Crew Manager",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Andy,
+    name: "Andy Banh",
+    title: "Dance Coordinator",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Kaelyn,
+    name: "Kaelyn Lam",
+    title: "Dance Coordinator",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Aidan,
+    name: "Aidan Lam",
+    title: "Esport Coordinator",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    // image: Andrew,
+    name: "Andrew Zhang",
+    title: "Sports Coordinator",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Caitlyn,
+    name: "Caitlyn Hang",
+    title: "Sports Coordinator",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Ryan_Nguyen,
+    name: "Ryan Nguyen",
+    title: "Philanthropy Chair",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+  {
+    image: Stephanie,
+    name: "Stephanie Huang",
+    title: "Academic Chair",
+    desc: "",
+    email: "",
+    excs: "",
+    instagram: "",
+  },
+];
