193bb787ec68ce24b0e2ccc78bde787b31d17af3
Brenda Rojas <broja016@ucr.edu>
2025-01-27 09:24:31 -0800

    Add 404 page text

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,9 @@
+import NotFoundText from "@/components/NotFoundText";
+
 const NotFound = () => {
   return (
     <div>
-      <div className="mt-[25vh] flex flex-col items-center justify-center text-center">
-        <p className="text-9xl font-bold text-csa-yellow-300">404</p>
-        <p className="font-lora text-2xl text-csa-gray-100">
-          page not found :(
-        </p>
-      </div>
+      <NotFoundText />
     </div>
   );
 };
---
+++ src/components/NotFoundText.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const NotFoundText = () => {
+  return (
+    <div className="mt-[25vh] flex flex-col items-center justify-center text-center">
+      <p className="text-9xl font-bold text-csa-yellow-300">404</p>
+      <p className="font-lora text-2xl text-csa-gray-100">page not found :(</p>
+    </div>
+  );
+};
+export default NotFoundText;
