a4c24309649ce55e77ecbfe6203161a25685e2d1
Ian Negishi <inegi001@ucr.edu>
2025-01-27 08:34:48 -0800

    comment fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,15 @@ import QuoteBox from "@/components/home/QuoteBox";
 import quotes from "@/data/quote-data";
 import Image from "next/image";
 import Dragon from "@/public/home/Dragon.png";
+import EHTitle from "@/components/events/EventHighlightsTitle";
+import MissionStatement from "@/components/home/MissionStatement";
 
 const Home = () => {
   return (
-    <div className="bg-csa-tan-100">
+    <div className="bg-csa-tan-100 pt-[12.2vh] md:pt-[14.4vh]">
       <Image src={Dragon} alt="dragon" className="left-0 top-0 w-1/4" />
+      <MissionStatement />
+      <EHTitle />
       <XinDescription />
       {quotes.map(({ quote, person }, index) => (
         <QuoteBox quote={quote} name={person} key={index} />
