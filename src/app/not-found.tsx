cc2b3779a91fe96d20eb644ec68fd29317c0ddf1
Patrick Wu <89278486+exitcy@users.noreply.github.com>
2025-01-27 22:16:36 -0800

    merged code

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,9 @@
 import NotFound from "@/components/NotFound";
-import Lantern from "@/components/404Lanterns";
 
 const NotFoundPage = () => {
   return (
     <div className="notfound-container">
       <NotFound />
-      <Lantern />
     </div>
   );
 };
--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,11 +1,34 @@
+import Image from "next/image";
+import Lantern from "@/public/notfound/lantern404.webp";
+
 const NotFound = () => {
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-csa-tan-100">
-      <p className="text-[12vw] font-bold text-csa-yellow-300">404</p>
-      <p className="font-lora text-[2.5vw] text-csa-gray-100">
+    <div className="relative flex h-screen flex-col items-center justify-center bg-csa-tan-100">
+      <Image
+        src={Lantern}
+        alt="Lantern background"
+        className="absolute opacity-100"
+        style={{
+          width: "40.40vw",
+          right: "11vw",
+          top: "0vh",
+          zIndex: 0,
+        }}
+      />
+      <p
+        className="text-[12vw] font-bold text-csa-yellow-300"
+        style={{ zIndex: 10 }}
+      >
+        404
+      </p>
+      <p
+        className="font-lora text-[2.5vw] text-csa-gray-100"
+        style={{ zIndex: 10 }}
+      >
         page not found :(
       </p>
     </div>
   );
 };
+
 export default NotFound;
