ba0a25e6a59d9bf162906efc97471ac66fbb8d40
Stanley Lew <stanley43595@gmail.com>
2025-01-27 17:09:08 -0800

    add bg color

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,6 +1,6 @@
 const NotFound = () => {
   return (
-    <div className="mt-[25vh] flex flex-col items-center justify-center text-center">
+    <div className="flex h-screen flex-col items-center justify-center bg-csa-tan-100">
       <p className="text-[12vw] font-bold text-csa-yellow-300">404</p>
       <p className="font-lora text-[2.5vw] text-csa-gray-100">
         page not found :(
