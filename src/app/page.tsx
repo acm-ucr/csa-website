1121906a96583cb5fb4c0b3016c954bc8b40e012
Nidu <niduwararahubedde@gmail.com>
2025-01-31 21:35:22 -0800

    Fixed the CSA header description and clouds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import XinDescription from "@/components/home/XinDescription";
 import QuoteBox from "@/components/home/QuoteBox";
 import quotes from "@/data/quote-data";
 import Image from "next/image";
+import clouds_home from "@/public/home/clouds_home.webp";
 import Dragon from "@/public/home/Dragon.webp";
 import EHTitle from "@/components/events/EventHighlightsTitle";
 import MissionStatement from "@/components/home/MissionStatement";
@@ -11,10 +12,15 @@ import MissionStatement from "@/components/home/MissionStatement";
 const Home = () => {
   return (
     <div className="bg-csa-tan-100 pt-[12.2vh] md:pt-[14.4vh]">
-      <Image src={Dragon} alt="dragon" className="left-0 top-0 w-1/4" />
-      <MissionStatement />
-      <EHTitle />
       <Header />
+      <Image
+        src={clouds_home}
+        alt="clouds on top of page"
+        className="absolute left-0 top-20 h-[263px] w-[900px] object-contain"
+      />
+      <MissionStatement />
+      <Image src={Dragon} alt="dragon" className="left-0 top-0 w-1/4" />
+      <EHTitle />
       <XinDescription />
       {quotes.map(({ quote, person }, index) => (
         <QuoteBox quote={quote} name={person} key={index} />
--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,22 +1,15 @@
 import React from "react";
-import Image from "next/image";
-import cloudsImage_csa from "@/public/home/cloudsImage_csa.svg"
 
 const Header = () => {
   return (
-    <div className="relative mx-auto h-[284px] max-w-xl border-r-8 border-csa-red-100 px-4 py-6 text-right">
-      <Image 
-        src={cloudsImage_csa} 
-        alt="front_page_clouds" 
-        className="absolute left-0 top-0 w-full h-[263px] object-cover" 
-      />
-      <p className="mb-8 font-bodoni text-2xl font-bold border-csa-red-100 md:text-6xl">
+    <div className="relative ml-auto mr-7 h-[284px] max-w-xl border-r-8 border-csa-red-100 px-4 py-6 text-right">
+      <p className="font-bodoni mb-8 border-csa-red-100 text-2xl font-bold text-csa-red-100 md:text-6xl">
         Chinese Student
         <br />
         Association
       </p>
 
-      <p className="text-4.5 max-3-3xl mx-auto font-Lora font-semibold leading-relaxed border-csa-gray-100 md:text-xl">
+      <p className="text-4.5 max-3-3xl font-Lora mx-auto font-semibold leading-relaxed text-csa-gray-100 md:text-xl">
         Social, cultural, and service oriented
         <br />
         organization that strives to promote the Chinese
