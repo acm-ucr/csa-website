3b054f8f4b38585658e513207d9257b039db48cb
xxiyun <wesleylwu@gmail.com>
2025-02-02 14:28:51 -0800

    fixed positioning of diamonds and board member images. added different color diamonds

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,13 +1,14 @@
-"use client";
 import BoardCard from "./BoardCard";
-import { boardInfo } from "@/data/boardInfo.ts";
+import { boardInfo } from "@/data/boardInfo";
 
 const Board = () => {
   return (
     <>
       <div className="mb-[16%] mt-[5%] grid grid-cols-3 gap-2 md:grid-cols-3">
-        {boardInfo.map((CARD) => (
+        {boardInfo.map((CARD, index) => (
           <BoardCard
+            key={index}
+            index={index}
             name={CARD.name}
             title={CARD.title}
             image={CARD.image}
---
+++ src/components/board/BoardCard.tsx
@@ -0,0 +1,58 @@
+import Image, { StaticImageData } from "next/image";
+import redDiamond from "@/public/board/redDiamond.webp";
+import brownDiamond from "@/public/board/brownDiamond.webp";
+import beigeDiamond from "@/public/board/beigeDiamond.webp";
+import greyDiamond from "@/public/board/greyDiamond.webp";
+
+const diamondImages = [redDiamond, greyDiamond, beigeDiamond, brownDiamond];
+
+interface BoardCardProps {
+  image: StaticImageData;
+  name: string;
+  title: string;
+  color: string;
+  index: number;
+}
+
+const BoardCard: React.FC<BoardCardProps> = ({
+  image,
+  name,
+  title,
+  color,
+  index,
+}) => {
+  const diamondColor =
+    diamondImages[Math.floor(index / 3) % diamondImages.length];
+  return (
+    <div className="">
+      <div className="relative flex justify-center">
+        <Image
+          src={diamondColor}
+          alt="redDiamond"
+          className=""
+          width={490}
+          height={490}
+        />
+        <Image
+          src={image}
+          alt="picture of person"
+          className="diamond absolute bottom-[19.4%] right-[31.7%]"
+          width={300}
+          height={300}
+        />
+      </div>
+      <p
+        className={
+          "relative bottom-7 flex justify-center text-5xl text-csa-red-100"
+        }
+      >
+        {name}
+      </p>
+      <p className="relative bottom-7 flex justify-center text-4xl text-csa-gray-100">
+        {title}
+      </p>
+    </div>
+  );
+};
+
+export default BoardCard;
