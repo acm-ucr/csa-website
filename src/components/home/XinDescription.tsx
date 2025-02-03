25a8c9ff4fd58f37ae60459a8b387ae54558ea52
Brandon Tran <brandontran1220@gmail.com>
2025-02-02 18:53:07 -0800

    Add mobile responsiveness of Xin description

--- src/components/home/XinDescription.tsx
+++ src/components/home/XinDescription.tsx
@@ -4,16 +4,20 @@ import xinImage from "@/public/home/xinimage.svg";
 
 const XinDescription = () => {
   return (
-    <div className="grid grid-cols-3 items-center border-y-8 border-csa-tan-300 bg-csa-green-100">
-      <p className="col-span-2 py-[6vh] pl-[4vw] text-left text-2xl text-white">
+    <div className="grid grid-cols-1 items-center border-y-8 border-csa-tan-300 bg-csa-green-100 md:grid-cols-3">
+      <p className="py-[6vh] pl-[4vw] text-left text-3xl text-white md:col-span-2 md:text-2xl">
         The Chinese character 心 ("xin") which stands for heart. The theme that
         we have been promoting is "Two Worlds, One Heart” which is a
         representation of our Chinese Heritage fused with the American Culture
         that defines the Chinese American.
       </p>
       <div className="relative flex items-center justify-center py-[6vh]">
-        <div className="aspect-square w-[16vw] rounded-full border-8 border-csa-tan-200 bg-csa-tan-100"></div>
-        <Image src={xinImage} alt="Xin_Image" className="absolute w-[12vw]" />
+        <div className="aspect-square w-[45vw] rounded-full border-8 border-csa-tan-200 bg-csa-tan-100 md:w-[16vw]"></div>
+        <Image
+          src={xinImage}
+          alt="Xin_Image"
+          className="absolute w-[35vw] md:w-[12vw]"
+        />
       </div>
     </div>
   );
