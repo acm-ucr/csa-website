d671986cb451c757f26665885afe28c75c70d5ab
Brandon Tran <brandontran1220@gmail.com>
2025-02-03 22:05:25 -0800

    format

--- src/components/home/XinDescription.tsx
+++ src/components/home/XinDescription.tsx
@@ -5,13 +5,13 @@ import xinImage from "@/public/home/xinimage.svg";
 const XinDescription = () => {
   return (
     <div className="grid grid-cols-1 items-center border-y-8 border-csa-tan-300 bg-csa-green-100 md:grid-cols-3">
-      <p className="md:py-[6vh] pt-[4vh] px-[4vh] md:pl-[4vw] text-left text-2xl text-white md:col-span-2 md:text-2xl">
+      <p className="px-[4vh] pt-[4vh] text-left text-2xl text-white md:col-span-2 md:py-[6vh] md:pl-[4vw] md:text-2xl">
         The Chinese character 心 ("xin") which stands for heart. The theme that
         we have been promoting is "Two Worlds, One Heart” which is a
         representation of our Chinese Heritage fused with the American Culture
         that defines the Chinese American.
       </p>
-      <div className="relative flex items-center justify-center md:py-[6vh] my-[6vh]">
+      <div className="relative my-[6vh] flex items-center justify-center md:py-[6vh]">
         <div className="aspect-square w-[45vw] rounded-full border-8 border-csa-tan-200 bg-csa-tan-100 md:w-[16vw]"></div>
         <Image
           src={xinImage}
