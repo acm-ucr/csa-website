a5528bda9cbafc4cad72df1cfb086cd7f3c2d40a
Phillip C <pcfan16@gmail.com>
2025-02-04 17:18:14 -0800

    Who we Are Issue

--- src/components/about/WhoWeAre.tsx
+++ src/components/about/WhoWeAre.tsx
@@ -4,9 +4,24 @@ const WhoWeAre = () => {
       <p className="text-left text-5xl font-medium text-csa-red-100">
         Who We Are
       </p>
+
       <div className="ml-2 mr-auto h-2 w-[9%] bg-csa-tan-300" />
+      <p className="text-left">
+        The Chinese Student Association (CSA) at UC Riverside is a social,
+        cultural, and service orientated organization that strives to promote
+        the Chinese American culture throughout the UCR and Riverside
+        communities. We provide various opportunities and events throughout the
+        year that allow students to develop leadership skills and also provide a
+        social outlet for students with similar backgrounds and interests. Our
+        organization is a great way to meet lifelong friends and create fond
+        memories that will contribute to a successful college experience and
+        beyond!{" "}
+      </p>
     </div>
   );
 };
 
 export default WhoWeAre;
+
+// npm i
+// npm run dev
