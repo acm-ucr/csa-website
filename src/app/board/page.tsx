681403fed62a487411514d7dbb3cf2bfed595e6e
xxiyun <wesleylwu@gmail.com>
2025-02-04 19:49:15 -0800

    Revert "added board title"
    This reverts commit 6107b68eea4732e9b7edc98f0a52e53b0133e723.

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,11 +1,8 @@
 import Board from "@/components/board/Board";
 import GroupPhoto from "@/components/board/GroupPhoto";
-import Title from "@/components/Title";
-
 const Page = () => {
   return (
     <div className="bg-csa-tan-100">
-      <Title text="board" />
       <Board />
       <GroupPhoto />
     </div>
