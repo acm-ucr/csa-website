4653cd217a4155e2a73bf2d2f35a35fcb07c8ebc
Nidu <niduwararahubedde@gmail.com>
2025-02-05 13:01:22 -0800

    trying to push to branch trial 1

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import Image from "next/image";
+import clouds_image from "public/home/clouds_home";
 
 const Header = () => {
   return (
     <div className="relative mr-[10vw] border-r-8 border-csa-red-100 pr-[2vw] text-right">
+      <Image
+        src={clouds_image}
+        alt="clouds on home page"
+        className="absolute left-0 top-7 h-[263px] w-[900px]"
+      />
       <div className="font-lora mb-8 flex flex-col border-csa-red-100 text-6xl font-bold text-csa-red-100">
         <p className="font-bold">Chinese Student</p>
         <p>Association</p>
