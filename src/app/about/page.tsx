7f8fecae3e68730ad770582799e60cb6e97eaffc
inegishi <inegishi5@gmail.com>
2025-02-05 22:47:33 -0800

    four pillar component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,6 @@
 import WhoWeAre from "@/components/about/WhoWeAre";
 import Heading from "@/components/about/FourPillTitle";
-import Title from "@/components/Title"; 
+import Title from "@/components/Title";
 import RightLantern from "@/components/about/RightLantern";
 import FourPillars from "@/components/about/FourPillars";
 const page = () => {
@@ -10,7 +10,7 @@ const page = () => {
       <Title text="about" />
       <WhoWeAre />
       <Heading />
-      <FourPillars/>
+      <FourPillars />
     </div>
   );
 };
