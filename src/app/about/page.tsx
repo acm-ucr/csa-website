a475d75175359bfc2aa29c7abfd73b0f019daa90
inegishi <inegishi5@gmail.com>
2025-02-05 22:47:03 -0800

    four pillar component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,7 @@
 import WhoWeAre from "@/components/about/WhoWeAre";
 import Heading from "@/components/about/FourPillTitle";
-import Title from "@/components/Title";
-import Social from "@/components/about/Social";
-import Academic from "@/components/about/AcademicComponent";
-import Culture from "@/components/about/Culture";
+import Title from "@/components/Title"; 
 import RightLantern from "@/components/about/RightLantern";
-import Philanthropy from "@/components/about/Philanthropy";
 import FourPillars from "@/components/about/FourPillars";
 const page = () => {
   return (
@@ -14,7 +10,7 @@ const page = () => {
       <Title text="about" />
       <WhoWeAre />
       <Heading />
-      <FourPillars />
+      <FourPillars/>
     </div>
   );
 };
