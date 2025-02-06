ff76123b2d618ae2847c6976b23357e12ef10723
Brenda Rojas <broja016@ucr.edu>
2025-02-06 11:40:49 -0800

    fix logo height

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,21 +6,21 @@ import { icons } from "@/data/footerData";
 
 const Footer = () => {
   return (
-    <div className="flex items-center border-t-8 border-csa-yellow-100 bg-csa-red-200">
+    <div className="flex flex-col items-center gap-4 border-t-8 border-csa-yellow-100 bg-csa-red-200 pb-4 md:flex-row md:gap-0 md:pb-0">
       <Link href="/">
         <Image
           src={CsaLogo}
           alt="CSA_Logo"
-          className="ml-5 w-[20vw] md:w-[6vw]"
+          className="mt-2 w-[22vw] md:ml-5 md:mt-0 md:w-[6vw]"
         />
       </Link>
 
-      <div className="absolute right-0 mr-8 flex gap-7 md:mr-6 md:gap-4">
+      <div className="] flex gap-4 md:absolute md:right-0 md:mr-6">
         {icons.map((icon, index) => (
           <Link
             href={icon.link}
             key={index}
-            className="w-[6vw] text-5xl text-white hover:scale-110 hover:text-csa-yellow-300 md:w-full"
+            className="text-2xl text-white hover:scale-110 hover:text-csa-yellow-300 md:text-5xl"
             target="_blank"
           >
             {icon.image}
