6cc2c7752ae11c4c3d43fdf77716c13761211bf5
Austin Le <ale193@ucr.edu>
2025-02-09 13:47:23 -0800

    Updating cherryTree

--- src/components/about/CherryTree.tsx
+++ src/components/about/CherryTree.tsx
@@ -5,26 +5,16 @@ import Tiger from "@/public/about/tiger.svg";
 
 const CherryTree = () => {
   return (
-    <div className="my-32 flex max-w-full flex-col">
-      <div className="overflow-hidden">
-        <div className="h-[1vh] w-[60vw] bg-csa-tan-300" />
-        <div className="relative -left-[16vw] -top-[44vh] md:-left-[4vw] md:-top-[40vh]">
-          <Image
-            src={Tree}
-            alt="Tree"
-            className="scale-[70%] transform md:scale-[90%]"
-          />
-        </div>
+    <div className="my-[30vh] flex flex-col">
+      <div className="h-[1vh] w-[60vw] bg-csa-tan-300" />
+      <div className="relative left-[0vw] top-[0vh]">
+        <Image src={Tree} alt="Tree" className="scale-[100%] transform" />
       </div>
       <div className="absolute right-0 overflow-hidden">
-        <div className="relative -bottom-[22vh] -right-[25vw] md:-bottom-[14vh] md:-right-[14vw]">
-          <Image
-            src={Tiger}
-            alt="Tiger"
-            className="scale-[50%] transform md:scale-[90%]"
-          />
+        <div className="relative -bottom-[14vh] -right-[14vw]">
+          <Image src={Tiger} alt="Tiger" className="scale-[90%] transform" />
         </div>
-        <div className="relative -right-[40vw] h-[1vh] w-[60vw] bg-csa-tan-300 md:right-0" />
+        <div className="h-[1vh] w-[60vw] bg-csa-tan-300" />
       </div>
     </div>
   );
