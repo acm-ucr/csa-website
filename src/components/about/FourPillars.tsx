36db8907d40a19bc2d4b42f9c0a8f0e03b827d7a
inegishi <inegishi5@gmail.com>
2025-02-10 23:21:40 -0800

    comment fix

--- src/components/about/FourPillars.tsx
+++ src/components/about/FourPillars.tsx
@@ -8,18 +8,18 @@ import gradCap from "@/public/about/gradCap.webp";
 const FourPillars = () => {
   return (
     <div className="grid grid-cols-2 justify-items-start gap-7 sm:gap-6 md:gap-20">
-      <div className="ml-5 relative col-span-2 flex h-[8vh] w-[64vw] sm:w-[48vw] items-center rounded-3xl border-8 border-csa-green-100 bg-white md:ml-20 md:h-[12vh] md:w-[42vw]">
-        <p className="font-lora p-4 lg:text-4xl text-3xl font-bold text-csa-gray-200">
+      <div className="relative col-span-2 ml-5 flex h-[8vh] w-[64vw] items-center rounded-3xl border-8 border-csa-green-100 bg-white sm:w-[48vw] md:ml-20 md:h-[12vh] md:w-[42vw]">
+        <p className="font-lora p-4 text-3xl font-bold text-csa-gray-200 lg:text-4xl">
           Philanthropy
         </p>
         <Image
           src={HeartImage}
           alt="heartImage"
-          className="absolute right-[-20%] top-[-100%] sm:right-[-20%] sm:top-[-130%] lg:top-[-120%] lg:w-[14vw] sm:w-[18vw] md:w-[18vw] w-[24vw] md:right-[-20%] md:top-[-60%]"
+          className="absolute right-[-20%] top-[-100%] w-[24vw] sm:right-[-20%] sm:top-[-130%] sm:w-[18vw] md:right-[-20%] md:top-[-60%] md:w-[18vw] lg:top-[-120%] lg:w-[14vw]"
         />
       </div>
-      <div className="ml-44 relative row-start-2 sm:ml-96 md:ml-2 flex sm:[60vw] h-[8vh] w-[50vw] sm:w-[42vw] md:w-[42vw] items-center rounded-3xl border-8 border-csa-gray-100 bg-white md:col-start-2 md:h-[12vh] ">
-        <p className="ml-6 lg:text-4xl text-3xl font-bold text-csa-gray-100 md:ml-8">
+      <div className="sm:[60vw] relative row-start-2 ml-44 flex h-[8vh] w-[50vw] items-center rounded-3xl border-8 border-csa-gray-100 bg-white sm:ml-96 sm:w-[42vw] md:col-start-2 md:ml-2 md:h-[12vh] md:w-[42vw]">
+        <p className="ml-6 text-3xl font-bold text-csa-gray-100 md:ml-8 lg:text-4xl">
           Social
         </p>
         <Image
@@ -29,22 +29,24 @@ const FourPillars = () => {
         />
       </div>
 
-      <div className="ml-5 relative col-span-2 flex h-[8vh] w-[64vw] sm:w-[48vw] items-center rounded-3xl border-8 border-csa-red-100 bg-white md:ml-20 md:h-[12vh] md:w-[42vw]">
-        <p className="font-lora lg:text-4xl absolute right-2 p-4 text-3xl font-bold text-csa-gray-200">
+      <div className="relative col-span-2 ml-5 flex h-[8vh] w-[64vw] items-center rounded-3xl border-8 border-csa-red-100 bg-white sm:w-[48vw] md:ml-20 md:h-[12vh] md:w-[42vw]">
+        <p className="font-lora absolute right-2 p-4 text-3xl font-bold text-csa-gray-200 lg:text-4xl">
           Culture
         </p>
         <Image
           src={redenvelope}
           alt="Red Envelope"
-          className="absolute ml-5 w-[30vw] md:w-[24vw] sm:w-[24vw] sm:translate-x-[-20%] lg:w-[14vw] translate-x-[-34%] translate-y-[5%] md:ml-3 "
+          className="absolute ml-5 w-[30vw] translate-x-[-34%] translate-y-[5%] sm:w-[24vw] sm:translate-x-[-20%] md:ml-3 md:w-[24vw] lg:w-[14vw]"
         />
       </div>
-      <div className="relative ml-24 col-start-1 sm:w-[48vw] sm:ml-80 md:ml-3 flex h-[8vh] w-[72vw] items-center rounded-3xl border-8 border-csa-yellow-100 bg-white md:col-start-2 md:h-[12vh] md:w-[40vw]">
-        <p className="ml-6 sm:ml-8  text-3xl font-bold lg:text-4xl text-csa-gray-100">Academic</p>
+      <div className="relative col-start-1 ml-24 flex h-[8vh] w-[72vw] items-center rounded-3xl border-8 border-csa-yellow-100 bg-white sm:ml-80 sm:w-[48vw] md:col-start-2 md:ml-3 md:h-[12vh] md:w-[40vw]">
+        <p className="ml-6 text-3xl font-bold text-csa-gray-100 sm:ml-8 lg:text-4xl">
+          Academic
+        </p>
         <Image
           src={gradCap}
           alt="gradCap"
-          className="absolute w-[42vw] translate-y-[-12%]  translate-x-[80%] sm:translate-x-[40%] sm:translate-y-[-9%] md:w-[28vw] md:translate-x-[60%] md:translate-y-[-9%] lg:translate-x-[120%] lg:w-[18vw]"
+          className="absolute w-[42vw] translate-x-[80%] translate-y-[-12%] sm:translate-x-[40%] sm:translate-y-[-9%] md:w-[28vw] md:translate-x-[60%] md:translate-y-[-9%] lg:w-[18vw] lg:translate-x-[120%]"
         />
       </div>
     </div>
