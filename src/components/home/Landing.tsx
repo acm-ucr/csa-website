a0001528163ac4785c0e837d1e0c74b49f9e75cc
Sookie Drabla <sookie.sd@@gmail.com>
2025-02-12 21:24:39 -0800

    fix responsiveness

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -5,41 +5,38 @@ const Landing = () => {
   return (
     <div>
       <div className="relative mr-[10vw] border-r-8 border-csa-red-100 pr-[2vw] text-right">
-        <div className="font-lora mb-8 flex flex-col border-csa-red-100 text-4xl font-semibold text-csa-red-100 sm:text-6xl">
+        <div className="font-lora mb-8 border-csa-red-100 text-right text-6xl font-semibold text-csa-red-100">
           <p className="font-bold">Chinese Student</p>
-          <p>Association</p>
+          <p className="font-bold">Association</p>
         </div>
-        <div className="font-lora flex flex-col text-2xl font-semibold leading-relaxed text-csa-gray-100">
-          <p>Social, cultural, and service oriented </p>
-          <p>organization that strives to promote the Chinese</p>
-          <p>American Culture throughout the UCR and</p>
-          <p>Riverside communities</p>
+        <div className="font-lora text-wrap pl-[30vw] text-3xl font-semibold leading-relaxed text-csa-gray-100">
+          <p>
+            Social, cultural, and service oriented organization that strives to
+            promote the Chinese American Culture throughout the UCR and
+            Riverside communities
+          </p>
         </div>
       </div>
-        <div className="relative my-4 sm:py-10 px-8 ml-[25vw] border-l-8 border-csa-tan-300 pl-6">
-          <p className="text-left text-4xl font-bold text-csa-red-100 sm:text-6xl">
-            Mission statement
-          </p>
-          <Image
-            src={RightCloud}
-            alt="right cloud"
-            className="absolute right-0 bottom-20"
-          />
-          <p className="mt-4 text-left text-2xl font-semibold leading-9 text-csa-gray-100">
-            Social, cultural, and service orientated
-            <br />
-            organization that strives to promote the Chinese
-            <br />
-            American culture throughout the UCR and
-            <br />
-            Riverside communities.
-          </p>
-        </div>
+      <div className="relative my-10 ml-[10vw] border-l-8 border-csa-tan-300 px-8 pl-6 sm:my-12 sm:ml-[25vw]">
+        <Image
+          src={RightCloud}
+          alt="right cloud"
+          className="absolute right-0 sm:bottom-[30%]"
+        />
+        <p className="text-left text-5xl font-bold text-csa-red-100 sm:text-left">
+          Mission statement
+        </p>
+        <p className="font-lora mt-4 pr-[17vw] text-left text-3xl font-semibold leading-9 text-csa-gray-100">
+          Social, cultural, and service orientated organization that strives to
+          promote the Chinese American culture throughout the UCR and Riverside
+          communities.
+        </p>
+      </div>
       <div>
         <Image
           src={Dragon}
           alt="dragon"
-          className="absolute left-0 top-12 w-1/4 pt-12 sm:py-11"
+          className="absolute left-0 top-12 w-[20vw] pt-12 sm:w-[1/4] sm:py-11"
         />
       </div>
     </div>
