a09192e7e20f8fc154c8e07b2cf15558d1f8a5b6
Sookie Drabla <sookie.sd@@gmail.com>
2025-02-18 20:29:22 -0800

    small fixes

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 const Landing = () => {
   return (
     <div>
-      <div className="px-25 relative mr-[10vw] border-r-8 border-csa-red-100 pr-5 text-right">
-        <div className="font-lora mb-8 border-csa-red-100 pl-20 text-right text-4xl font-semibold text-csa-red-100 sm:text-5xl">
+      <div className="relative mr-[10vw] border-r-8 border-csa-red-100 pr-5 text-right">
+        <div className="font-lora mb-8 flex flex-col border-csa-red-100 pl-[25%] text-right text-4xl font-semibold text-csa-red-100 sm:text-5xl">
           <p className="font-bold">Chinese Student</p>
           <p className="font-bold">Association</p>
         </div>
@@ -17,14 +17,14 @@ const Landing = () => {
           </p>
         </div>
       </div>
-      <div className="relative my-10 ml-[10vw] border-l-8 border-csa-tan-300 px-8 pl-8 sm:my-12 sm:ml-[35vw]">
+      <div className="relative ml-[15vw] border-l-8 border-csa-tan-300 px-10 pl-5 sm:my-12 sm:ml-[30vw]">
         <Image
           src={RightCloud}
           alt="right cloud"
           className="absolute bottom-10 right-0 sm:bottom-[30%]"
         />
-        <p className="text-left text-3xl font-bold text-csa-red-100 sm:text-left sm:text-3xl">
-          Mission statement
+        <p className="text-left text-3xl font-bold text-csa-red-100 sm:text-3xl">
+          Mission statememt
         </p>
         <p className="font-lora text:2xl mt-4 pr-[17vw] text-left font-semibold leading-relaxed text-csa-gray-100 sm:text-2xl">
           Social, cultural, and service orientated organization that strives to
@@ -36,7 +36,7 @@ const Landing = () => {
         <Image
           src={Dragon}
           alt="dragon"
-          className="sm:top-15% absolute left-0 top-10 w-[25%] pt-20 sm:w-[20%]"
+          className="sm:top-15% absolute left-0 top-[12%] w-[27%] sm:w-[20%]"
         />
       </div>
     </div>
