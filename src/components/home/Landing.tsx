1d800b8aeb9507d883d86a62f9f8814521285424
Sookie Drabla <sookie.sd@@gmail.com>
2025-02-18 20:49:07 -0800

    typo and more small fixes

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -9,7 +9,7 @@ const Landing = () => {
           <p className="font-bold">Chinese Student</p>
           <p className="font-bold">Association</p>
         </div>
-        <div className="font-lora text:2xl text-wrap pl-[30vw] font-semibold leading-relaxed text-csa-gray-100 sm:text-3xl">
+        <div className="font-lora text-wrap pl-[30vw] text-xl font-semibold leading-relaxed text-csa-gray-100 sm:text-3xl">
           <p>
             Social, cultural, and service oriented organization that strives to
             promote the Chinese American Culture throughout the UCR and
@@ -23,10 +23,10 @@ const Landing = () => {
           alt="right cloud"
           className="absolute bottom-10 right-0 sm:bottom-[30%]"
         />
-        <p className="text-left text-3xl font-bold text-csa-red-100 sm:text-3xl">
-          Mission statememt
+        <p className="text-left text-3xl font-bold text-csa-red-100 sm:text-4xl">
+          Mission statement
         </p>
-        <p className="font-lora text:2xl mt-4 pr-[17vw] text-left font-semibold leading-relaxed text-csa-gray-100 sm:text-2xl">
+        <p className="font-lora mt-4 pr-[17vw] text-left text-xl font-semibold leading-relaxed text-csa-gray-100 sm:text-2xl">
           Social, cultural, and service orientated organization that strives to
           promote the Chinese American culture throughout the UCR and Riverside
           communities.
@@ -36,7 +36,7 @@ const Landing = () => {
         <Image
           src={Dragon}
           alt="dragon"
-          className="sm:top-15% absolute left-0 top-[12%] w-[27%] sm:w-[20%]"
+          className="absolute left-0 top-[12%] w-[27%] sm:w-[20%]"
         />
       </div>
     </div>
