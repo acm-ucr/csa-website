de23a06b3c9af47666ac9a0297ec7f46b85815d5
wesley <wesleylwu@gmail.com>
2025-02-20 13:37:27 -0800

    fixed sizing

--- src/components/events/EventCard.tsx
+++ src/components/events/EventCard.tsx
@@ -6,33 +6,29 @@ const EventCard = () => {
   return (
     <div className="relative left-[16%] flex">
       <div className="relative left-[4%] flex">
-        <div className="relative top-[11%]">
+        <div className="relative top-[14%] overflow-hidden">
           <Image
             src={whiteRectangle}
             alt="whiteRectangle"
-            className="relative left-[32%] h-auto w-auto scale-125"
+            className="relative left-[5%]"
           />
-          <p className="absolute left-1/2 top-1/2 -translate-y-40 translate-x-10 text-8xl font-bold text-csa-yellow-100">
+          <p className="absolute left-1/2 top-1/2 -translate-x-32 -translate-y-32 text-7xl font-bold text-csa-yellow-100">
             Name
           </p>
-          <p className="absolute left-1/2 top-1/2 -translate-y-9 translate-x-10 text-5xl text-csa-gray-100">
+          <p className="absolute left-1/2 top-1/2 -translate-x-32 -translate-y-9 text-5xl text-csa-gray-100">
             location
           </p>
         </div>
       </div>
 
-      <div className="relative right-[33%]">
+      <div className="relative right-[70%]">
         <div className="relative">
-          <Image
-            src={redDiamond}
-            alt="redDiamond"
-            className="relative scale-125"
-          />
-          <p className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-36 text-center text-8xl font-bold text-csa-yellow-100">
+          <Image src={redDiamond} alt="redDiamond" className="relative" />
+          <p className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-24 text-center text-7xl font-bold text-csa-yellow-100">
             Oct
             <br /> 17
           </p>
-          <p className="absolute left-1/2 top-1/2 -translate-x-1/2 translate-y-16 whitespace-nowrap text-5xl font-bold text-white">
+          <p className="absolute left-1/2 top-1/2 -translate-x-1/2 translate-y-16 whitespace-nowrap text-4xl font-bold text-white">
             12:00 pm
           </p>
         </div>
@@ -44,7 +40,7 @@ const EventCard = () => {
 const EventCardsList = () => {
   const cards = [1, 2, 3];
   return (
-    <div className="flex flex-col space-y-[5%]">
+    <div className="flex flex-col space-y-[2%]">
       {" "}
       {}
       {cards.map((_, index) => (
