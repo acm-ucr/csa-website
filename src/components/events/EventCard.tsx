aca07839e322e1923c0b4cba2ba74f7a6524987c
wesley <wesleylwu@gmail.com>
2025-02-24 14:42:29 -0800

    removed images, removed absolute, cleaned up

--- src/components/events/EventCard.tsx
+++ src/components/events/EventCard.tsx
@@ -1,53 +1,24 @@
-import Image from "next/image";
-import redDiamond from "@/public/events/redDiamond.webp";
-import whiteRectangle from "@/public/events/whiteRectangle.webp";
-
 const EventCard = () => {
   return (
-    <div className="relative left-[16%] flex">
-      <div className="relative left-[4%] flex">
-        <div className="relative top-[14%] overflow-hidden">
-          <Image
-            src={whiteRectangle}
-            alt="whiteRectangle"
-            className="relative left-[5%]"
-          />
-          <p className="absolute left-1/2 top-1/2 -translate-x-32 -translate-y-32 text-7xl font-bold text-csa-yellow-100">
-            Name
-          </p>
-          <p className="absolute left-1/2 top-1/2 -translate-x-32 -translate-y-9 text-5xl text-csa-gray-100">
-            location
-          </p>
-        </div>
+    <div className="relative left-[28%] flex">
+      <div className="relative h-[17vw] w-[50vw] bg-white">
+        <p className="pl-[25%] pt-[8%] text-7xl font-bold text-csa-yellow-100">
+          Name
+        </p>
+        <p className="pl-[25%] text-5xl text-csa-gray-100">location</p>
       </div>
 
-      <div className="relative right-[70%]">
-        <div className="relative">
-          <Image src={redDiamond} alt="redDiamond" className="relative" />
-          <p className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-24 text-center text-7xl font-bold text-csa-yellow-100">
-            Oct
-            <br /> 17
-          </p>
-          <p className="absolute left-1/2 top-1/2 -translate-x-1/2 translate-y-16 whitespace-nowrap text-4xl font-bold text-white">
-            12:00 pm
-          </p>
-        </div>
+      <div className="relative right-[60%] h-[18vw] w-[18vw] rotate-45 bg-csa-red-200">
+        <p className="-rotate-45 pr-[25%] pt-[10%] text-center text-7xl font-bold text-csa-yellow-100">
+          Oct
+          <br /> 17
+        </p>
+        <p className="-rotate-45 whitespace-nowrap pl-[39%] pt-[10%] text-4xl font-bold text-white">
+          12:00 pm
+        </p>
       </div>
     </div>
   );
 };
 
-const EventCardsList = () => {
-  const cards = [1, 2, 3];
-  return (
-    <div className="flex flex-col space-y-[2%]">
-      {" "}
-      {}
-      {cards.map((_, index) => (
-        <EventCard key={index} />
-      ))}
-    </div>
-  );
-};
-
-export default EventCardsList;
+export default EventCard;
