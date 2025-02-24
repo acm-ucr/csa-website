59eace26bd17f58e2770f359c635d1e51d503df4
Patrick Wu <89278486+exitcy@users.noreply.github.com>
2025-02-24 13:28:52 -0800

    fixed mobilie to space out better and better fitment overall

--- src/components/board/BoardCard.tsx
+++ src/components/board/BoardCard.tsx
@@ -16,6 +16,7 @@ interface BoardCardProps {
 const BoardCard: React.FC<BoardCardProps> = ({ image, name, title, index }) => {
   const diamondColor =
     diamondImages[Math.floor(index / 3) % diamondImages.length];
+
   return (
     <div className="flex flex-col text-center">
       <div className="relative flex justify-center">
@@ -23,17 +24,13 @@ const BoardCard: React.FC<BoardCardProps> = ({ image, name, title, index }) => {
         <Image
           src={image}
           alt="picture of person"
-          className="diamond absolute bottom-[19%] right-[18.2%] w-[20vw] rotate-[.5deg] 2xl:bottom-[19.2%] 2xl:right-[21.5%] 2xl:w-[18vw]"
+          className="diamond xs:w-[35vw] absolute bottom-[19%] right-[18.2%] w-[20vw] rotate-[.5deg] sm:w-[30vw] md:w-[24vw] 2xl:bottom-[19.2%] 2xl:right-[21.5%] 2xl:w-[18vw]"
         />
       </div>
-      <p
-        className={
-          "relative bottom-7 flex justify-center text-5xl text-csa-red-100"
-        }
-      >
+      <p className="relative bottom-7 flex justify-center text-3xl text-csa-red-100 md:text-4xl lg:text-5xl">
         {name}
       </p>
-      <p className="relative bottom-4 flex justify-center text-4xl text-csa-gray-100">
+      <p className="relative bottom-4 flex justify-center text-2xl text-csa-gray-100 md:text-3xl lg:text-4xl">
         {title}
       </p>
     </div>
