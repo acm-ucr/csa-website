dcec4cec5f211421e18664f6740eb69f89c33b38
Simon Cun <simoncun2586@gmail.com>
2025-03-03 12:08:54 -0800

    removed all of the md and 2xl and fixed responsiveness with vw, vh, and % values

--- src/components/events/EventCard.tsx
+++ src/components/events/EventCard.tsx
@@ -2,20 +2,20 @@ const EventCard = () => {
   return (
     <div className="relative left-[28%] flex">
       <div className="relative h-[17vw] w-[50vw] bg-white">
-        <p className="pl-[30%] pt-[7%] text-[4vw] font-bold text-csa-yellow-100 md:text-[4vw] 2xl:text-7xl">
+        <p className="pl-[30%] pt-[9%] text-[5vw] font-bold text-csa-yellow-100 leading-[75%]">
           Name
         </p>
-        <p className="pl-[30%] text-[3vw] text-csa-gray-100 md:text-[3vw] 2xl:text-5xl">
+        <p className="pl-[30%] text-[3vw] text-csa-gray-100">
           location
         </p>
       </div>
 
-      <div className="relative right-[60%] h-[18vw] w-[18vw] rotate-45 bg-csa-red-200">
-        <p className="-rotate-45 pr-[30%] pt-[10%] text-center text-[4vw] font-bold leading-[110%] text-csa-yellow-100 md:text-[3.5vw] 2xl:text-7xl">
+      <div className="relative right-[60%] h-[18vw] w-[18vw] rotate-45 bg-csa-red-200 text-center">
+        <p className="-rotate-45 pr-[27%] pt-[10%] text-[4.5vw] font-bold leading-[100%] text-csa-yellow-100">
           Oct
           <br /> 17
         </p>
-        <p className="-rotate-45 whitespace-nowrap pl-[39%] pt-[12%] text-center text-[2vw] font-bold text-white 2xl:text-4xl">
+        <p className="-rotate-45 whitespace-nowrap pl-[43%] pt-[12%] text-[2.3vw] font-bold text-white">
           12:00 pm
         </p>
       </div>
