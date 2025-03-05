1bac27ba8d02ea50bf0c274f0a02ce1309506054
Draylend <draylend0821@gmail.com>
2025-03-04 19:54:59 -0800

    Edit Tiger for mobile responsiveness
    * Added mobile responsiveness for Tiger on About page
    * Fixed mobile responsiveness for 4 Pillars

--- src/components/about/CherryTree.tsx
+++ src/components/about/CherryTree.tsx
@@ -5,16 +5,18 @@ import Tiger from "@/public/about/tiger.webp";
 
 const CherryTree = () => {
   return (
-    <div className="my-[30vh] flex flex-col">
+    <div className="my-[10vh] flex flex-col">
       <div className="h-[1vh] w-[60vw] bg-csa-tan-300" />
       <div className="relative left-[0vw] top-[0vh]">
         <Image src={Tree} alt="Tree" className="scale-[100%] transform" />
       </div>
-      <div className="absolute right-0 overflow-hidden">
-        <div className="relative -bottom-[15vh] -right-[17vw]">
+      <div className="absolute right-0 h-full overflow-hidden">
+        <div className="md:-right-[] relative -bottom-[29vh] -right-[20vw] md:-bottom-[26vh] lg:-bottom-[6vh] lg:-right-[17vw]">
           <Image src={Tiger} alt="Tiger" className="scale-[60%] transform" />
         </div>
-        <div className="relative -right-[12vw] h-[1vh] w-[60vw] bg-csa-tan-300" />
+      </div>
+      <div className="relative mt-[20vh] lg:mt-[15vh]">
+        <div className="absolute right-0 h-[1vh] w-[60vw] bg-csa-tan-300" />
       </div>
     </div>
   );
