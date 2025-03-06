45e04e1d1a239ca906025dd56b1c48b48c2e2fb3
xxiyun <wesleylwu@gmail.com>
2025-03-05 21:44:17 -0800

    prettier

--- src/components/events/EventCard.tsx
+++ src/components/events/EventCard.tsx
@@ -14,7 +14,7 @@ const EventCard: React.FC<EventCardProps> = ({
   month,
   day,
   time,
-  index=0 //temporary hard code num
+  index = 0, //temporary hard code num
 }) => {
   const diamondColor = colors[index % colors.length];
   return (
@@ -24,8 +24,9 @@ const EventCard: React.FC<EventCardProps> = ({
           {name}
         </p>
         <p className="pl-[35%] text-[3vw] text-csa-gray-100">{location}</p>
-        <div className={`absolute left-[-10%] top-0 h-[18vw] w-[18vw] rotate-45 bg-${diamondColor} text-center border`}>
-
+        <div
+          className={`absolute left-[-10%] top-0 h-[18vw] w-[18vw] rotate-45 bg-${diamondColor} border text-center`}
+        >
           <p className="-rotate-45 pr-[27%] pt-[10%] text-[4.5vw] font-bold leading-none text-csa-yellow-100">
             {month}
             <br /> {day}
