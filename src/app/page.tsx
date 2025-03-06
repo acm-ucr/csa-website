665179ec3b81e47afd64428668c8caf52c501cca
felix diaz <felixmdm321@gmail.com>
2025-03-05 23:56:58 -0800

    shadcn carousel, mobile responsive as far as I can tell lol

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import XinDescription from "@/components/home/XinDescription";
 import EHTitle from "@/components/events/EventHighlightsTitle";
 import Landing from "@/components/home/Landing";
 import Quotes from "@/components/home/Quote";
+import CarouselHome from "@/components/CarouselHome";
 
 const Page = () => {
   return (
-    <div className="bg-csa-tan-400 pt-[12.2vh] md:pt-[14.4vh]">
+    <div className="overflow-hidden bg-csa-tan-400 pt-[12.2vh] md:pt-[14.4vh]">
       <Landing />
       <XinDescription />
+      <CarouselHome />
       <EHTitle />
       <Quotes />
     </div>
