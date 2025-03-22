72917ea72d3a2d08d38b5312194db8d0f7a30b65
stanleylew5 <stanley43595@gmail.com>
2025-03-21 22:06:04 -0700

    format

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -4,7 +4,7 @@ import CalendarCall from "@/components/events/CalendarCall";
 
 const Page = () => {
   return (
-    <div className="bg-csa-tan-100 min-h-screen">
+    <div className="min-h-screen bg-csa-tan-100">
       <FruitBranch />
       <Title>events</Title>
       <CalendarCall />
