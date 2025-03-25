5739bdf5d29e13aabfe090d783bc491b9ae1248e
Aryan Goel <aryangoel27@gmail.com>
2025-03-25 14:41:04 -0700

    Compressed images and edited events/FruitBranch.tsx  * changing fruitBranch from svg to webp needed changes to responsiveness

--- src/components/home/XinDescription.tsx
+++ src/components/home/XinDescription.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import xinImage from "@/public/home/xinimage.svg";
+import xinImage from "@/public/home/xinimage.webp";
 
 const XinDescription = () => {
   return (
