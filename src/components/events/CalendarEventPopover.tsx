26d60ac099116241663c8493ed44ee00460b7de1
wesley <wesleylwu@gmail.com>
2025-03-25 13:44:49 -0700

    adjust calendar event sizing

--- src/components/events/CalendarEventPopover.tsx
+++ src/components/events/CalendarEventPopover.tsx
@@ -64,7 +64,7 @@ const CalendarEventPopover = ({
           {title}
         </p>
       </PopoverTrigger>
-      <PopoverContent className="w-[30vw] 2xl:w-[20vw]">
+      <PopoverContent className="w-[15vw] text-center 2xl:w-[20vw]">
         <p className="bg-csa-green-100 px-[1vw] py-[1vh] text-[10px] text-csa-yellow-100 sm:text-xs md:text-lg 2xl:text-xl">
           {date.toLocaleString("default", {
             year: "numeric",
@@ -75,8 +75,8 @@ const CalendarEventPopover = ({
         </p>
         {(location || hasStartTime || description) && (
           <div className="flex flex-col gap-y-[1vh] bg-csa-tan-500 py-[1vh] pl-[2vw] text-[10px] text-csa-gray-100 sm:text-xs md:text-lg 2xl:text-xl">
-            <p>{location}</p>
-            <p>
+            <p className="mr-[13%]">
+              {location}
               {hasStartTime && (
                 <>
                   {formattedStartHour}:{formattedStartMinutes} {startHourSuffix}
@@ -88,8 +88,9 @@ const CalendarEventPopover = ({
                   )}
                 </>
               )}
+              <br />
+              {description}
             </p>
-            <p>{description}</p>
           </div>
         )}
       </PopoverContent>
